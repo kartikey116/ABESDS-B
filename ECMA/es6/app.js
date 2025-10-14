dcd68139272fda56aff37552553d9e0477b4786a
KARTIKEY <kartikeyu07@gmail.com>
2025-10-14 12:17:29 +0530

    react

---
+++ ECMA/es6/app.js
@@ -0,0 +1,6 @@
+import greet from './greet.js'; 
+import { add, subtract } from './math.js'; 
+
+console.log(greet('Kartikey'));       
+console.log(add(5, 3));               
+console.log(subtract(10, 4));         
---
+++ ECMA/es6/index.js
@@ -0,0 +1,6 @@
+const div = document.createElement('div');
+const h1 = document.createElement('h1');
+h1.innerText = 'hello my name is kartikey and i am a student of abesec';
+div.appendChild(h1);
+const root = document.getElementById('root');
+root.appendChild(div);
